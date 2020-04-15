8d4d506f9478f4a5d797a61a5c90a56bb9a1edbd
Megan Comeau <mlcomeau99@gmail.com>
2020-04-15 18:07:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_value = 0
-  lowest_key = nil 
+  lowest_key = nil
 
   name_hash.each do |key, value|
     if lowest_value == 0 || value < lowest_value
@@ -12,5 +12,5 @@ def key_for_min_value(name_hash)
     end
   end
   lowest_key
-  
+
 end
